b1cb7991134127f8fadb7311f2ca5b1af48b7630
Juan Manuel Damas <juan.damas.mtz@gmail.com>
2026-02-27 18:49:21 -0600

    Configuration, PropertySources, UTF-8, Spring Expression Language (SpEL), Map SpEL con @Value, Environment

--- src/main/java/com/jmdcode/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/jmdcode/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,9 +1,12 @@
 package com.jmdcode.curso.springboot.webapp.springboot_web.controllers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -29,6 +32,30 @@ public class PathVariableController {
 
     @Value("${config.last-name}")
     private String lastName;
+
+    @Value("${config.country.name}")
+    private String countryName;
+
+    @Value("${config.country.code}")
+    private String countryCode;
+
+    @Value("${config.list.values}")
+    private List<String> listOfValues;
+
+    @Value("#{'${config.list.values}'.split(',')}")
+    private List<String> valueList;
+
+    @Value("#{'${config.list.values}'.toUpperCase()}")
+    private String valueString;
+
+    @Value("#{${config.valuesMap}}")
+    private Map<String, Object> valuesMap;
+
+    @Value("#{${config.valuesMap}.product}")
+    private String product;
+
+    @Autowired
+    private Environment environment;
 
     @GetMapping("/baz/{message}")
     public ParamDto baz(@PathVariable String message) {
@@ -56,10 +83,20 @@ public class PathVariableController {
     @GetMapping("/get-user-values")
     public Map<String, Object> getUser(@Value("${config.code}") Long id) {
         Map<String, Object> response = new HashMap<>();
-        response.put("id", id);
+        // response.put("id", Integer.valueOf(environment.getProperty("config.code")));
+        response.put("id", environment.getProperty("config.code", Long.class));
         response.put("username", username);
         response.put("name", name);
         response.put("lastName", lastName);
+        response.put("country", countryName);
+        response.put("countryCode", countryCode);
+        response.put("listOfValues", listOfValues);
+        response.put("valueList", valueList);
+        response.put("valueString", valueString);
+        response.put("valuesMap", valuesMap);
+        response.put("product", product);
+        response.put("username2", environment.getProperty("config.username"));
+
         return response;
     }
 
